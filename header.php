9d8170b6b34a6242bef4f7a26dc560c4643bbedf
stapxs <1007028430.stapx@gmail.com>
2023-08-08 09:16:56 +0800

    摸大鱼 :bug: 修正顶栏头像在未登录时显示错误的问题，同时在窄布局下登陆后也会显示用户名而不是登录文本

--- header.php
+++ header.php
@@ -62,9 +62,13 @@
             </div>
             <form class="form-inline my-2 my-lg-0" id="search" method="post" action="<?php $this->options->siteUrl(); ?>" role="search">
                 <input class="form-control mr-sm-2 bar-search" type="text" id="s" name="s" class="text" placeholder="搜索" aria-label="搜索">
-                <div id="user-avatar" class="avatar" title="登录" onclick="jumpTo('<?php $this->options->adminUrl(); ?>', true)">
+                <div id="user-avatar" class="avatar" title="<?php $this->user->hasLogin() ? $this->user->screenName() : '登录'; ?>" onclick="jumpTo('<?php $this->options->adminUrl(); ?>', true)">
+                <?php if($this->user->hasLogin()): ?>
                     <?php echo $this->user->gravatar(330, 'G', NULL, NULL) ?>
-                    <span id="user-name" class="user-name">登录</span>
+                <?php else: ?>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
+                <?php endif; ?>
+                    <span id="user-name" class="user-name"><?php $this->user->hasLogin() ? $this->user->screenName() : '登录'; ?></span>
                 </div>
             </form>
         </div>
